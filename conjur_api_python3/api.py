2a3658915523c01c7ac5998f6c546a3e5fcbfc94
Srdjan Grubor <sgnn7@sgnn7.org>
2019-02-11 09:20:48 -0600

    Genericized the http endpoint invocation
    Lots of duplicated code removed from HTTTP invocation code by
    genericizing the requests module activation.

--- conjur_api_python3/api.py
+++ conjur_api_python3/api.py
@@ -1,8 +1,13 @@
 import base64
+from enum import auto, Enum
 from urllib.parse import quote
 
 import requests
 from requests.auth import HTTPBasicAuth
+
+class HttpVerb(Enum):
+    GET = auto()
+    POST = auto()
 
 class Api(object):
     KIND_VARIABLE='variable'
@@ -45,7 +50,7 @@ class Api(object):
         url = "{url}/authn/{account}/login".format(url=self._url,
                                                    account=quote(self._account))
         print("Authenticating to {}...".format(url))
-        return self._get(url, auth=HTTPBasicAuth(login_id, password)).text
+        return self._invoke_endpoint(HttpVerb.GET, url, auth=HTTPBasicAuth(login_id, password)).text
 
     def authenticate(self, login_id=None, api_key=None):
         """
@@ -60,7 +65,7 @@ class Api(object):
                                                                   account=quote(self._account),
                                                                   login=quote(login_id))
         print("Authenticating to {}...".format(url))
-        return self._post(url, api_key).text
+        return self._invoke_endpoint(HttpVerb.POST, url, api_key).text
 
     def set_variable(self, variable_id, value, login_id, api_key):
         token = self.authenticate(login_id, api_key)
@@ -68,7 +73,7 @@ class Api(object):
                                                                    account=quote(self._account),
                                                                    kind=self.KIND_VARIABLE,
                                                                    identifier=quote(variable_id))
-        return self._post(url, value, api_token=token).text
+        return self._invoke_endpoint(HttpVerb.POST, url, value, api_token=token).text
 
     def get_variable(self, variable_id, login_id, api_key):
         token = self.authenticate(login_id, api_key)
@@ -76,33 +81,20 @@ class Api(object):
                                                                    account=quote(self._account),
                                                                    kind=self.KIND_VARIABLE,
                                                                    identifier=quote(variable_id))
-        return self._get(url, api_token=token).content
+        return self._invoke_endpoint(HttpVerb.GET, url, api_token=token).content
 
     def _base64encode(self, source_str):
         return base64.b64encode(source_str.encode())
 
-    def _get(self, url, *args, check_errors=True, auth=None, api_token=None):
+    def _invoke_endpoint(self, verb_id, url, *args, check_errors=True, auth=None, api_token=None):
         headers={}
         if api_token and len(api_token) > 0:
             encoded_token = self._base64encode(api_token).decode('utf-8')
             headers['Authorization'] = 'Token token="{}"'.format(encoded_token)
 
-
-        response = requests.get(url, *args, verify=self._ssl_verify, auth=auth, headers=headers)
-        if check_errors and response.status_code >= 300:
-            # TODO: Use custom errors
-            raise RuntimeError("Request failed: {}: {}".format(response.status_code,
-                                                               response.text))
-
-        return response
-
-    def _post(self, url, *args, check_errors=True, auth=None, api_token=None):
-        headers={}
-        if api_token and len(api_token) > 0:
-            encoded_token = self._base64encode(api_token).decode('utf-8')
-            headers['Authorization'] = 'Token token="{}"'.format(encoded_token)
-
-        response = requests.post(url, *args, verify=self._ssl_verify, auth=auth, headers=headers)
+        verb = HttpVerb(verb_id).name.lower()
+        request_method = getattr(requests, verb)
+        response = request_method(url, *args, verify=self._ssl_verify, auth=auth, headers=headers)
         if check_errors and response.status_code >= 300:
             # TODO: Use custom errors
             raise RuntimeError("Request failed: {}: {}".format(response.status_code,
