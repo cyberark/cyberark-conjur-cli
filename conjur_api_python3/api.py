dbd62404daa37fed7bad851f94e0f7f4c8e0fe42
Srdjan Grubor <sgnn7@sgnn7.org>
2019-02-11 11:48:27 -0600

    Genericized url invocation pattern
    This reduces boilerplate by a signifficant amount and fixes a lot of the
    issues related to scalability of endpoint invocations.

--- conjur_api_python3/api.py
+++ conjur_api_python3/api.py
@@ -10,6 +10,8 @@ class HttpVerb(Enum):
     POST = auto()
 
 class ConjurEndpoint(Enum):
+    AUTHENTICATE = "{url}/authn/{account}/{login}/authenticate"
+    LOGIN = "{url}/authn/{account}/login"
     SECRETS = "{url}/secrets/{account}/{kind}/{identifier}"
 
 class Api(object):
@@ -55,10 +57,9 @@ class Api(object):
             # TODO: Use custom error
             raise RuntimeError("Missing parameters in login invocation!")
 
-        url = "{url}/authn/{account}/login".format(url=self._url,
-                                                   account=quote(self._account))
-        print("Authenticating to {}...".format(url))
-        return self._invoke_endpoint(HttpVerb.GET, url, auth=HTTPBasicAuth(login_id, password)).text
+        print("Logging in to {}...".format(self._url))
+        return self._invoke_endpoint(HttpVerb.GET, ConjurEndpoint.LOGIN,
+                None, auth=HTTPBasicAuth(login_id, password)).text
 
     def authenticate(self, login_id=None, api_key=None):
         """
@@ -69,11 +70,9 @@ class Api(object):
             # TODO: Use custom error
             raise RuntimeError("Missing parameters in authentication invocation!")
 
-        url = "{url}/authn/{account}/{login}/authenticate".format(url=self._url,
-                                                                  account=quote(self._account),
-                                                                  login=quote(login_id))
-        print("Authenticating to {}...".format(url))
-        return self._invoke_endpoint(HttpVerb.POST, url, api_key).text
+        print("Authenticating to {}...".format(self._url))
+        return self._invoke_endpoint(HttpVerb.POST, ConjurEndpoint.AUTHENTICATE,
+                { 'login': quote(login_id) }, api_key).text
 
     def set_variable(self, variable_id, value, login_id, api_key):
         token = self.authenticate(login_id, api_key)
@@ -82,9 +81,9 @@ class Api(object):
             'kind': self.KIND_VARIABLE,
             'identifier': quote(variable_id)
         }
-        url = ConjurEndpoint.SECRETS.value.format(**self._default_params, **params)
 
-        return self._invoke_endpoint(HttpVerb.POST, url, value, api_token=token).text
+        return self._invoke_endpoint(HttpVerb.POST, ConjurEndpoint.SECRETS, params,
+                                     value, api_token=token).text
 
     def get_variable(self, variable_id, login_id, api_key):
         token = self.authenticate(login_id, api_key)
@@ -93,14 +92,21 @@ class Api(object):
             'kind': self.KIND_VARIABLE,
             'identifier': quote(variable_id)
         }
-        url = ConjurEndpoint.SECRETS.value.format(**self._default_params, **params)
 
-        return self._invoke_endpoint(HttpVerb.GET, url, api_token=token).content
+        return self._invoke_endpoint(HttpVerb.GET, ConjurEndpoint.SECRETS, params,
+                                     api_token=token).content
 
     def _base64encode(self, source_str):
         return base64.b64encode(source_str.encode())
 
-    def _invoke_endpoint(self, verb_id, url, *args, check_errors=True, auth=None, api_token=None):
+    def _invoke_endpoint(self, verb_id, endpoint_id, params, *args,
+            check_errors=True, auth=None, api_token=None):
+
+        if params is None:
+            params = {}
+
+        url = ConjurEndpoint(endpoint_id).value.format(**self._default_params, **params)
+
         headers={}
         if api_token and len(api_token) > 0:
             encoded_token = self._base64encode(api_token).decode('utf-8')
@@ -108,6 +114,7 @@ class Api(object):
 
         verb = HttpVerb(verb_id).name.lower()
         request_method = getattr(requests, verb)
+
         response = request_method(url, *args, verify=self._ssl_verify, auth=auth, headers=headers)
         if check_errors and response.status_code >= 300:
             # TODO: Use custom errors
