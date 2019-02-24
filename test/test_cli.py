5315cdd611479050bb7e0aaf87ffe570fbb1d8ff
Srdjan Grubor <sgnn7@sgnn7.org>
2019-02-24 15:44:32 -0600

    Simplified cli test decorator
    We didn't need the name of the program in most cases.

--- test/test_cli.py
+++ test/test_cli.py
@@ -11,15 +11,14 @@ from conjur_api_python3.cli import Cli
 from conjur_api_python3.version import __version__
 
 class CliTest(unittest.TestCase):
-
-    def cli_test(cli_args, *args, **kwargs):
+    def cli_test(cli_args=[]):
         def test_cli_decorator(original_function):
             @wraps(original_function)
             def test_wrapper_func(self, *inner_args, **inner_kwargs):
                 capture_stream = io.StringIO()
                 with self.assertRaises(SystemExit) as sys_exit:
                     with redirect_stdout(capture_stream):
-                        with patch.object(sys, 'argv', cli_args):
+                        with patch.object(sys, 'argv', ["cli"] + cli_args):
                             Cli().run()
 
                 self.assertEqual(sys_exit.exception.code, 0)
@@ -28,23 +27,23 @@ class CliTest(unittest.TestCase):
             return test_wrapper_func
         return test_cli_decorator
 
-    @cli_test(["cli"])
+    @cli_test()
     def test_cli_without_args_shows_help(self, output):
         self.assertIn("usage: cli", output)
 
-    @cli_test(["cli", "-h"])
+    @cli_test(["-h"])
     def test_cli_shows_help_with_short_help_flag(self, output):
         self.assertIn("usage: cli", output)
 
-    @cli_test(["cli", "--help"])
+    @cli_test(["--help"])
     def test_cli_shows_help_with_long_help_flag(self, output):
         self.assertIn("usage: cli", output)
 
-    @cli_test(["cli", "-v"])
+    @cli_test(["-v"])
     def test_cli_shows_version_with_short_version_flag(self, output):
         self.assertEquals("cli v{}\n".format(__version__), output)
 
-    @cli_test(["cli", "--version"])
+    @cli_test(["--version"])
     def test_cli_shows_version_with_long_version_flag(self, output):
         self.assertEquals("cli v{}\n".format(__version__), output)
 
