dcd3a9b77df53dadd05ab6ecd176a925ab2860bc
Srdjan Grubor <sgnn7@sgnn7.org>
2019-03-15 15:08:07 -0500

    Converted to using instance vars for integration test params
    Since we don't use the annotation anymore to fill the CLI params, we can
    directly use an instance var.

--- test/test_cli_integration.py
+++ test/test_cli_integration.py
@@ -6,6 +6,7 @@ from .util.cli_helpers import integration_test, invoke_cli
 
 from conjur_api_python3.version import __version__
 
+
 class CliIntegrationTest(unittest.TestCase):
     REQUIRED_ENV_VARS = {
         'CONJUR_APPLIANCE_URL': 'url',
@@ -14,21 +15,20 @@ class CliIntegrationTest(unittest.TestCase):
         'CONJUR_AUTHN_API_KEY': 'api-key',
     }
 
-    # Dynamically create a static param list that is class-wide
-    cli_auth_params = ['--debug']
-    for required_env_var, param_name in REQUIRED_ENV_VARS.items():
-        cli_auth_params += ['--{}'.format(param_name), os.environ[required_env_var]]
-
-
     def setUp(self):
+        self.cli_auth_params = ['--debug']
         for required_env_var, param_name in self.REQUIRED_ENV_VARS.items():
             self.assertIsNotNone(os.environ[required_env_var],
                     'ERROR: {} env var must be available for this test to work!'.format(required_env_var))
             self.assertGreater(len(os.environ[required_env_var]), 0,
                     'ERROR: {} env var must have a valid value for this test to work!'.format(required_env_var))
 
+        # Dynamically create a static param list that is class-wide
+        for required_env_var, param_name in self.REQUIRED_ENV_VARS.items():
+            self.cli_auth_params += ['--{}'.format(param_name), os.environ[required_env_var]]
+
         # Reset variable "one/password"
-        set_params = CliIntegrationTest.cli_auth_params + \
+        set_params = self.cli_auth_params + \
             ["variable", "set", "one/password", "garbagevalue"]
         invoke_cli(self, set_params)
 
@@ -37,11 +37,11 @@ class CliIntegrationTest(unittest.TestCase):
         expected_value='expected ' + uuid.uuid4().hex
 
         # Implicitly relies on the setter
-        set_params = CliIntegrationTest.cli_auth_params + \
+        set_params = self.cli_auth_params + \
             ["variable", "set", "one/password", expected_value]
         invoke_cli(self, set_params)
 
-        set_params = CliIntegrationTest.cli_auth_params + \
+        set_params = self.cli_auth_params + \
             ["variable", "get", "one/password"]
         output = invoke_cli(self, set_params)
 
