20f35402a6eccbddff9f340386cfe6bc845ee43b
Srdjan Grubor <sgnn7@sgnn7.org>
2019-04-12 14:08:57 -0500

    Added ability to test client parameters from CLI
    This will allow us to ensure that the api invocations are doing what we
    are expecting them to do with the Client class.

--- test/util/cli_helpers.py
+++ test/util/cli_helpers.py
@@ -4,7 +4,7 @@ import sys
 from contextlib import redirect_stdout
 from functools import wraps
 
-from unittest.mock import patch
+from unittest.mock import patch, MagicMock
 
 from conjur_api_python3.cli import Cli
 
@@ -37,15 +37,18 @@ def cli_test(cli_args=[], integration=False):
         @wraps(original_function)
         def test_wrapper_func(self, *inner_args, **inner_kwargs):
             capture_stream = io.StringIO()
+            client_instance_mock = MagicMock()
 
             with self.assertRaises(SystemExit) as sys_exit:
                 with redirect_stdout(capture_stream):
-                    with patch.object(sys, 'argv', ["cli"] + cli_args):
+                    with patch.object(sys, 'argv', ["cli"] + cli_args), \
+                            patch('conjur_api_python3.cli.Client') as mock_client:
+                        mock_client.return_value = client_instance_mock
                         Cli().run()
 
             self.assertEqual(sys_exit.exception.code, 0,
                 "ERROR: CLI returned an unexpected error status code: '{}'".format(cli_command))
-            original_function(self, cli_command, capture_stream.getvalue())
+            original_function(self, cli_command, capture_stream.getvalue(), client_instance_mock)
 
         # Set integration flag if specified
         test_wrapper_func.integration = integration
