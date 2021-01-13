6b78b9153d2d314f67194c2ca24b726b0a5f2738
mbenita-Cyberark <76062747+mbenita-Cyberark@users.noreply.github.com>
2021-01-13 10:59:17 +0200

    Correct flow to run integration tests as a process (#144)
    This PR merges the final fix to run be able to run the integration tests in the different platforms we support. This will increase our productivity and release process because we don't need to manually run our integration tests.

--- test/util/test_runners/integrations_tests_runner.py
+++ test/util/test_runners/integrations_tests_runner.py
@@ -11,7 +11,11 @@ Roadmap:
 # Builtins
 import sys
 
+sys.path.append('.')
+sys.path.append('..')
 # Third party
+import uuid
+import unittest.mock
 import unittest
 import base64
 import requests
@@ -25,6 +29,7 @@ from test.test_integration_cli import CliIntegrationTest
 from test.test_integration_configurations import CliIntegrationTestConfigurations
 from test.test_integration_credentials import CliIntegrationTestCredentials
 from test.test_integration_list import CliIntegrationListTest
+from test.test_integration_oss import CliIntegrationTestOSS
 from test.util.test_runners.integration_test_case import IntegrationTestCaseBase
 from test.util.test_runners.test_runner_args import TestRunnerArgs
 from test.util.test_runners.params import ClientParams, TestEnvironmentParams
@@ -36,22 +41,21 @@ def main():
     args = TestRunnerArgs.create_from_args()
 
     runner = TestRunner(args)
-    runner.start_env()
-    runner.run_tests(test_modules=[CliIntegrationTest,
-                                   CliIntegrationTestConfigurations,
-                                   CliIntegrationTestCredentials,
-                                   CliIntegrationListTest])
 
+    test_to_run_list=[CliIntegrationTest,
+                      CliIntegrationTestConfigurations,
+                      CliIntegrationTestCredentials,
+                      CliIntegrationListTest]
+    if args.run_oss_tests:
+        test_to_run_list.append(CliIntegrationTestOSS)
+    runner.run_tests(test_modules=test_to_run_list)
 
 class TestRunner:  # pragma: no cover
 
     def __init__(self, args: TestRunnerArgs):
         self.runner_args = args
-        path_provider = TestRunnerPathProvider(helpers_file_dir=args.files_folder)
         self.api_key = self.__get_api_key()
-        self.env_params = TestEnvironmentParams(args.cli_to_test, args.invoke_cli_as_process, path_provider)
-        self.client_params = ClientParams(url=self.runner_args.hostname, account=self.runner_args.account,
-                                          login=self.runner_args.login, api_key=self.api_key)
+        self.start_env()
 
     def run_tests(self, test_modules: list):
         """
@@ -69,16 +73,19 @@ class TestRunner:  # pragma: no cover
         """
         set up the test environment
         Test environment setup will be in the following order:
-        1) Create a netrc file to provide login details
-        2) Run conjur init to initialize the CLI and get the certificate and conjurrc files
+        1) Run conjur init to initialize the CLI and get the certificate and conjurrc files
+        1) Run login a login to CLI and generate netrc file
         """
 
-        # copy netrc from home folder to test folder
-        path_provider = TestRunnerPathProvider.getInstance()
+        path_provider = TestRunnerPathProvider(file_helper_dir=self.runner_args.files_folder)
+        self.__create_conjurrc_file()
         self.__create_netrc_file()
-        shutil.copy(path_provider.netrc_path,
-                    os.path.join(path_provider.helpers_files_path, path_provider.netrc_file_name))
+        self.env_params = TestEnvironmentParams(self.runner_args.cli_to_test, self.runner_args.invoke_cli_as_process, path_provider)
+        self.client_params = ClientParams(url=self.runner_args.hostname, account=self.runner_args.account,
+                                          login=self.runner_args.login, api_key=self.api_key)
 
+    def __create_conjurrc_file(self):
+        path_provider = TestRunnerPathProvider.getInstance()
         proc = sb.Popen(
             [self.runner_args.working_cli_path_only_for_init, "init", "-a", f"{self.runner_args.account}", "-u",
              f"{self.runner_args.hostname}",
@@ -93,16 +100,22 @@ class TestRunner:  # pragma: no cover
         shutil.copy(path_provider.conjurrc_path, path_provider.test_conjurrc_file_path)
 
     def __create_netrc_file(self):
-        password = self.api_key
-        home_path = str(Path.home())
+        path_provider = TestRunnerPathProvider.getInstance()
+        proc = sb.Popen(
+            [self.runner_args.working_cli_path_only_for_init, "login", "-n", f"{self.runner_args.login}", "-p",
+             f"{self.runner_args.password}"],
+            stdin=sb.PIPE,
+            stdout=sb.PIPE,
+            stderr=sb.PIPE)
+        output = proc.communicate(timeout=10)[0]
+        if proc.returncode != 0:
+            raise RuntimeError(f"Error Login: {output}")
 
-        with open(os.path.join(home_path, DEFAULT_NETRC_FILE), 'w') as f:
-            f.write(f"machine {self.runner_args.hostname}/authn\n")
-            f.write(f"login {self.runner_args.login}\n")
-            f.write(f"password {password}\n")
+        shutil.copy(path_provider.netrc_path,
+            os.path.join(path_provider.helpers_files_path, path_provider.netrc_file_name))
 
     def __get_api_key(self):
-        url = f"{self.runner_args.hostname}/authn/cucumber/login"
+        url = f"{self.runner_args.hostname}/authn/{self.runner_args.account}/login"
         encode = base64.b64encode(f"admin:{self.runner_args.password}".encode('utf-8')).decode('utf-8')
         headers = {'Authorization': f'Basic {encode}'}
 
@@ -118,6 +131,7 @@ class TestRunner:  # pragma: no cover
         """
         test_names = self.__get_relevant_tests_for_class(test_cases_class)  # we filter by test_name_identifier
         suite = unittest.TestSuite()
+        print("Now running: ", test_cases_class.__name__)
         for test_name in test_names:
             specific_test = test_cases_class(test_name, self.client_params, self.env_params)
             suite.addTest(specific_test)
--- test/util/test_runners/params.py
+++ test/util/test_runners/params.py
@@ -1,5 +1,5 @@
 import os
-from test.util.test_path_provider import TestRunnerPathProvider, TestIntegrationPathProvider
+from test.util.test_path_provider import TestRunnerPathProvider
 
 
 class ClientParams:
@@ -16,6 +16,7 @@ class ClientParams:
         # when we run the tests using nose2 script we will use the CONJUR_AUTHN_API_KEY env variable
         self.env_api_key = api_key if api_key else os.environ['CONJUR_AUTHN_API_KEY']
 
+
 class TestEnvironmentParams:
     """
     DTO wrapped the environmentParams params
@@ -24,7 +25,12 @@ class TestEnvironmentParams:
     """
 
     def __init__(self, cli_to_test_path=None, invoke_process=False,
-                 path_provider=TestIntegrationPathProvider()):
+                 path_provider=None):
         self.invoke_process = invoke_process
         self.cli_to_test_path = cli_to_test_path
-        self.path_provider = path_provider
+        if path_provider:
+            self.path_provider = path_provider
+        elif TestRunnerPathProvider.getInstance():
+            self.path_provider = TestRunnerPathProvider.getInstance()
+        else:
+            self.path_provider = TestRunnerPathProvider()
