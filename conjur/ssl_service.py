25580f2c82229b2fbe9e7c35a854e6702b0e6478
sigalsax <ssax18@gmail.com>
2021-01-28 13:14:25 +0200

    Expand TLS version communication to 1.2 and 1.3
    Previously we were only limited to communicate over TLS 1.2 to fetch the certificate from the Conjur server. This changes expands this to also support 1.3 and block anything less than 1.2. This option can now be configured.

--- conjur/ssl_service.py
+++ conjur/ssl_service.py
@@ -7,9 +7,16 @@ This module is for all SSL operations
 """
 # Third party
 import logging
+import os
 import socket
 from OpenSSL import SSL
 from OpenSSL.crypto import FILETYPE_PEM, dump_certificate
+
+_tls_methods = {
+    "1.0": SSL.TLSv1_METHOD,
+    "1.1": SSL.TLSv1_1_METHOD,
+    "1.2": SSL.TLSv1_2_METHOD
+}
 
 # pylint: disable=too-few-public-methods
 class SSLService:
@@ -37,7 +44,22 @@ class SSLService:
         """
         Method for opening a socket to the Conjur server
         """
-        ctx = SSL.Context(method=SSL.TLSv1_2_METHOD)
+        # Makes the TLS version configurable through an ENV variable
+        if os.getenv('TLS_VERSION') in _tls_methods:
+            ctx = SSL.Context(method=_tls_methods[os.getenv('TLS_VERSION')])
+        else:
+            # Despite its name, SSLv23_METHOD is the alias for PROTOCOL_TLS which allows for
+            # negotiation between different TLS versions and chooses the highest supported server
+            # protocol. See https://docs.python.org/3/library/ssl.html#ssl.PROTOCOL_SSLv23
+            # specifically PROTOCOL_SSLv23 and PROTOCOL_TLS
+            ctx = SSL.Context(method=SSL.SSLv23_METHOD)
+            # Using SSLv23 by default enables all types of negotiation and we want to limit
+            # negotiation to only 1.2 and 1.3 versions
+            ctx.set_options(SSL.OP_NO_SSLv2)
+            ctx.set_options(SSL.OP_NO_SSLv3)
+            ctx.set_options(SSL.OP_NO_TLSv1)
+            ctx.set_options(SSL.OP_NO_TLSv1_1)
+
         # We set the following to false because we want to fetch the
         # certificates and not use them to validate yet
         ctx.check_hostname = False
