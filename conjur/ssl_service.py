7f2f1cf708968b5a805067dcb5aa2fb28f77844b
Sigal Sax <ssax18@gmail.com>
2021-01-28 17:15:02 +0200

    Expand TLS version communication to 1.2 and 1.3 (#154)
    This PR allows the ability to establish a TLS connection with Conjur to fetch the certificates over TLS v1.3 and 1.2. Previously this was only limited to TLS v1.2. The version is also now configurable through an environment variable CONJUR_TLS_VERSION

--- conjur/ssl_service.py
+++ conjur/ssl_service.py
@@ -7,9 +7,17 @@ This module is for all SSL operations
 """
 # Third party
 import logging
+import os
 import socket
 from OpenSSL import SSL
 from OpenSSL.crypto import FILETYPE_PEM, dump_certificate
+
+_conjur_tls_methods = {
+    "1.0": SSL.TLSv1_METHOD,
+    "1.1": SSL.TLSv1_1_METHOD,
+    "1.2": SSL.TLSv1_2_METHOD,
+    "1.3": SSL.SSLv23_METHOD
+}
 
 # pylint: disable=too-few-public-methods
 class SSLService:
@@ -37,11 +45,29 @@ class SSLService:
         """
         Method for opening a socket to the Conjur server
         """
-        ctx = SSL.Context(method=SSL.TLSv1_2_METHOD)
-        # We set the following to false because we want to fetch the
-        # certificates and not use them to validate yet
-        ctx.check_hostname = False
-        ctx.verify_mode = False
+        tls_version=os.getenv('CONJUR_TLS_VERSION')
+        # Makes the TLS version configurable through an ENV variable
+        if tls_version in _conjur_tls_methods:
+            # pylint: disable=logging-fstring-interpolation
+            logging.debug("'CONJUR_TLS_VERSION' environment variable supplied. Establishing TLS "
+                          f"v{tls_version} connection...")
+            ctx = SSL.Context(method=_conjur_tls_methods[tls_version])
+            if tls_version == '1.3':
+                # disables all TLS versions expect for 1.3
+                cls.disable_tls_versions(ctx, support_1_3=True)
+        else:
+            if tls_version:
+                logging.debug("Warning: 'CONJUR_TLS_VERSION' environment variable supplied but "
+                              "not in correct format (Example: CONJUR_TLS_VERSION=1.2)")
+            # Despite its name, SSLv23_METHOD is the alias for PROTOCOL_TLS which allows for
+            # negotiation between different TLS versions and chooses the highest supported server
+            # protocol. See https://docs.python.org/3/library/ssl.html#ssl.PROTOCOL_SSLv23
+            # specifically PROTOCOL_SSLv23 and PROTOCOL_TLS
+            ctx = SSL.Context(method=SSL.SSLv23_METHOD)
+            # Using SSLv23 by default enables all types of negotiation and we want to limit
+            # negotiation to only 1.2 and 1.3 versions
+            cls.disable_tls_versions(ctx)
+
         # pylint: disable = line-too-long
         # Taken from https://gist.github.com/brandond/f3d28734a40c49833176207b17a44786#file-sslscan-py-L17
         conjur_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -53,3 +79,17 @@ class SSLService:
                       "Fetching certificate from Conjur server")
 
         return conjur_sock
+
+    @classmethod
+    def disable_tls_versions(cls, ctx, support_1_3=False):
+        """
+        Method for disabling TLS/SSL versions
+        """
+        if support_1_3:
+            # disable TLS 1.2 when user requires only 1.3
+            ctx.set_options(SSL.OP_NO_TLSv1_2)
+
+        ctx.set_options(SSL.OP_NO_SSLv2)
+        ctx.set_options(SSL.OP_NO_SSLv3)
+        ctx.set_options(SSL.OP_NO_TLSv1)
+        ctx.set_options(SSL.OP_NO_TLSv1_1)
