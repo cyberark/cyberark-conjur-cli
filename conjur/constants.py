67e9c98a4b38d987e1afa6b7e62e251a22c95937
ehadar <eran.hadar@cyberark.com>
2021-03-30 17:40:20 +0300

    Address PR comments Add comment

--- conjur/constants.py
+++ conjur/constants.py
@@ -37,4 +37,5 @@ KEYSTORE_ATTRIBUTES = [MACHINE, LOGIN, PASSWORD]
 # For testing purposes
 TEST_HOSTNAME = "https://conjur-https"
 
+# For user interaction
 LOGIN_IS_REQUIRED = "To start using the CLI, log in to Conjur"
