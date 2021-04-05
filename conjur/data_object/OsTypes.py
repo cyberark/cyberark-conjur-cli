2738b0ff35f79e9ffa248d058924c4e242606e36
Matan Benita <matan.benita@cyberark.com>
2021-04-05 17:00:01 +0300

    rename os_types

--- conjur/data_object/OsTypes.py
+++
@@ -1,8 +0,0 @@
-from enum import Enum
-
-
-class OsTypes(Enum):
-    UNKNOWN = 0
-    WINDOWS = 1
-    MAC_OS = 2
-    LINUX = 3
---
+++ conjur/util/os_types.py
@@ -0,0 +1,17 @@
+"""
+OSTypes module
+
+This module is used to represent different os platforms.
+"""
+
+from enum import Enum
+
+
+class OSTypes(Enum):
+    """
+    Represent possible platforms that the cli might be running on
+    """
+    UNKNOWN = 0
+    WINDOWS = 1
+    MAC_OS = 2
+    LINUX = 3
