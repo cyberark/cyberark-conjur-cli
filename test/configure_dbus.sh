e6a292085197ec52b6435d50ee6a43e6b8204327
ehadar <eran.hadar@cyberark.com>
2021-04-12 20:02:56 +0300

    Disable fips

--- test/configure_dbus.sh
+++ test/configure_dbus.sh
@@ -38,8 +38,9 @@ elif [ "$SERVER_MODE" == "appliance" ]; then
   echo "echo fips check..." >> /dbus.sh
   echo "fips-mode-setup --check" >> /dbus.sh
   echo "echo disable fips..." >> /dbus.sh
-  echo "grubby --update-kernel=ALL --remove-args=fips=1" >> /dbus.sh
-  echo "[[ -f /etc/default/grub ]] && sed -i 's/ fips=1//' /etc/default/grub" >> /dbus.sh
+  echo "fips-mode-setup --disable --no-bootcfg" >> /dbus.sh
+  #echo "grubby --update-kernel=ALL --remove-args=fips=1" >> /dbus.sh
+  #echo "[[ -f /etc/default/grub ]] && sed -i 's/ fips=1//' /etc/default/grub" >> /dbus.sh
   echo "echo fips check..." >> /dbus.sh
   echo "fips-mode-setup --check" >> /dbus.sh
   echo "echo running tests..." >> /dbus.sh
