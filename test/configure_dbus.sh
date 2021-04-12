1ee9c7c75dfd888377de3a668f4dab0b2a427670
ehadar <eran.hadar@cyberark.com>
2021-04-12 16:31:32 +0300

    Add debug msgs

--- test/configure_dbus.sh
+++ test/configure_dbus.sh
@@ -4,18 +4,22 @@ echo "Configuring D-bus environment"
 
 echo "#!/bin/bash -ex" >> /dbus.sh
 echo "# D-bus environment script" /dbus.sh
+echo "echo ->0" >> /dbus.sh
 echo eval "$(dbus-launch --sh-syntax)" > /dbus.sh
+echo "echo ->1" >> /dbus.sh
 # Create D-bus keyring directories
 mkdir -p ~/.cache
 mkdir -p ~/.local/share/keyrings
 # Unlock D-bus with empty password
 echo "ps -ef | grep dbus | grep -v grep" >> /dbus.sh
+echo "echo ->2" >> /dbus.sh
 echo eval "$(echo | gnome-keyring-daemon --unlock)" >> /dbus.sh
+echo "echo ->3" >> /dbus.sh
 # Export D-bus variables to working shell to support keyring
 echo "export DBUS_SESSION_BUS_ADDRESS" >> /dbus.sh
 echo "export GNOME_KEYRING_CONTROL" >> /dbus.sh
 echo "export SSH_AUTH_SOCK" >> /dbus.sh
-
+echo "echo ->4" >> /dbus.sh
 if [ "$DEBUG" == "true" ]; then
   echo "bash" >> /dbus.sh
 elif [ "$SERVER_MODE" == "appliance" ]; then
@@ -33,7 +37,10 @@ elif [ "$SERVER_MODE" == "appliance" ]; then
 
   echo "echo running tests..." >> /dbus.sh
   echo "$_cmd" >> /dbus.sh
+  echo "echo ->5" >> /dbus.sh
   echo "echo running tests completed..." >> /dbus.sh
+  echo "echo ->6" >> /dbus.sh
+  cat /dbus.sh
 else
   echo "bash -c \"nose2 -v -X --config integration_test.cfg -A 'integration'\"" >> /dbus.sh
 fi
