00167ee3a60481ff387fdb71cbca11763fa28b11
eranha <eran.hadar@cyberark.com>
2021-10-25 18:12:49 +0300

    List - build basic skeleton
    code skeleton to the extended flags in the list command.

--- conjur/api/endpoints.py
+++ conjur/api/endpoints.py
@@ -30,4 +30,4 @@ class ConjurEndpoint(Enum):
     HOST_FACTORY_REVOKE_TOKEN = "{url}/host_factory_tokens/{token}"
     HOST_FACTORY_HOSTS = "{url}/host_factories/hosts"
     ROLES_MEMBERS_OF = "{url}/roles/{account}/{kind}/{identifier}?members"
-    RESOURCES_MEMBERS_OF = "{url}/resources/{account}/{kind}/{identifier}??permitted_roles=true&privilege={privilege}"
+    RESOURCES_MEMBERS_OF = "{url}/resources/{account}/{kind}/{identifier}?permitted_roles=true&privilege={privilege}"
